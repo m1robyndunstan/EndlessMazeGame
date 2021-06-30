5469103e4e3b7e49c1a1c5b7bf71e7655b26e597
Robyn <Robyn@192.168.1.162>
2021-06-30 11:56:22 -0400

    Initialize angular project

---
+++ endless-maze/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageHeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
