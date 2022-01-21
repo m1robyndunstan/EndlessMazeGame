19684735874dbc3f055b5cc43cfc2a4597264fd6
Robyn Dunstan <DunstanR@members1st.org>
2022-01-21 15:43:03 -0500

    On click, intro goes to interface

--- endless-maze-game/src/app/maze-intro/maze-intro.component.ts
+++ endless-maze-game/src/app/maze-intro/maze-intro.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MazeScreen } from '../MazeScreen';
+import { MazeState } from '../MazeState';
 
 @Component({
   selector: 'app-maze-intro',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MazeIntroComponent implements OnInit {
 
+  @Input() currentGame?: MazeState;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  enterMaze(): void {
+    if (this.currentGame) {
+      this.currentGame.currentScreen = MazeScreen.Interface;
+    }
   }
 
 }
