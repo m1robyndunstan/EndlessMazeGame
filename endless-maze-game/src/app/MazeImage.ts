5f037b879c7c4232aeded2c329934a51e80784bf
Robyn Dunstan <dunstanr@members1st.org>
2022-04-12 14:42:14 -0400

    Add paint version exit door

--- endless-maze-game/src/app/MazeImage.ts
+++ endless-maze-game/src/app/MazeImage.ts
@@ -1,22 +1,4 @@
 export enum MazeImage {
-    OldPathLeft = 'assets/img/path/path_left_color.png',
-    OldWallLeft = 'assets/img/path/wall_left_color.png',
-    OldPathCenter = 'assets/img/path/path_center_color.png',
-    OldWallCenter = 'assets/img/path/wall_center_color.png',
-    OldPathRight = 'assets/img/path/path_right_color.png',
-    OldWallRight = 'assets/img/path/wall_right_color.png',
-
-    // Exit
-    OldExitSpecialLeft = 'assets/img/path/exit_special_left_color.png',
-    OldExitPathLeft = 'assets/img/path/exit_path_left_color.png',
-    OldExitWallLeft = 'assets/img/path/exit_wall_left_color.png',
-    OldExitSpecialCenter = 'assets/img/path/exit_special_center_color.png',
-    OldExitPathCenter = 'assets/img/path/exit_path_center_color.png',
-    OldExitWallCenter = 'assets/img/path/exit_wall_center_color.png',
-    OldExitSpecialRight = 'assets/img/path/exit_special_right_color.png',
-    OldExitPathRight = 'assets/img/path/exit_path_right_color.png',
-    OldExitWallRight = 'assets/img/path/exit_wall_right_color.png',
-
     // Path Image Components
     GrassFloor = 'assets/img/path/grass_full_length.jpg',
     BushPathCenter = 'assets/img/path/path_center_bush.png',
@@ -31,5 +13,8 @@ export enum MazeImage {
     BushWallRight = 'assets/img/path/wall_right_bush.png',
 
     // Exit Image Components
-    StoneFloor = 'assets/img/path/stones_full_length.jpg'
+    StoneFloor = 'assets/img/path/stones_full_length.jpg',
+    ExitCenter = 'assets/img/path/exit_center.png',
+    ExitLeft = 'assets/img/path/exit_left.png',
+    ExitRight = 'assets/img/path/exit_right.png'
 }
