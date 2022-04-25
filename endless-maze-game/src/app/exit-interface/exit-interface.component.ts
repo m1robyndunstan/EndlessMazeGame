e9540ba5c199117df01b1921276655166ca7afc7
Robyn Dunstan <DunstanR@members1st.org>
2022-04-25 14:08:17 -0400

    Use exit image and tweak some wording

--- endless-maze-game/src/app/exit-interface/exit-interface.component.ts
+++ endless-maze-game/src/app/exit-interface/exit-interface.component.ts
@@ -14,12 +14,12 @@ export class ExitInterfaceComponent implements OnInit {
   description: string;
   doorIsLocked: boolean;
 
-  defaultText: string = "The closed wooden door is inset into the hedge. There is a sign beside the door that says \"Return book here\" with a slot beneath it.";
+  defaultText: string = "The closed wooden door is inset into the hedge. There is a sign beside the door that says \"Return book here\" with a flap beneath it.";
   examineDoorText: string = "The door is well-built with no cracks between the boards. There is no keyhole to see through. The frame seems to have grown into the hedge. There is no way to go around the door or to tell what is on the other side.";
   knockText: string = "You knock firmly on the door. Nothing happens.";
   lockedText: string = "You try to open the door, but it doesn't move at all. It is clearly locked.";
   breakDownText: string = "You back up several feet and run at the door as fast as you can. You slam into the door and stagger back, stunned. The door is completely unharmed.";
-  examineSlotText: string = "The slot is about a foot long and a few inches tall. It is completely dark inside, and you can't feel anything interesting inside it. Your book will easily fit in this slot."
+  examineSlotText: string = "The flap is about a foot wide and a several inches tall. It is completely dark inside, and you can't feel anything interesting inside it. Your book will fit easily."
   returnBookText: string = "The book disappears into the slot. You hear a click from behind the door.";
 
   constructor() { 
