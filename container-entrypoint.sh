b3bad26b69cda11ee2d268c26513da0ccf43d397
Robyn Dunstan <dunstanr@members1st.org>
2022-04-26 13:17:09 -0400

    More changes to make docker work

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+pwd
 # check to see if we need to run yarn
-if [ ! -d endless-maze-game/node_modules ] || [ ! -L ebdless-maze-game/node_modules] ; then
-    (cd endless-maze-game ; ln -s ../node_modules .)
+if [ ! -d /app/endless-maze-game/node_modules ] || [ ! -L /app/endless-maze-game/node_modules] ; then
+    (cd /app/endless-maze-game ; ln -s ../node_modules .)
 fi
 
-pwd
+ls
 echo "change directory"
-cd endless-maze-game
+cd /app/endless-maze-game
 pwd
 echo "run server and watch"
-yarn run watch
+ng serve --host 0.0.0.0 --watch
